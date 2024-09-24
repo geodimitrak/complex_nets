a57d0a19bcf5d967e6885730c01d8b628c077afa
geodimitrak <geo.dimitrakopoulos@gmail.com>
2024-09-24 13:29:25 +0300

    Add files via upload

--- code-MATLAB/network_metrics.m
+++ code-MATLAB/network_metrics.m
@@ -1,40 +1,47 @@
 % Authors: Aristidis G. Vrahatis, Georgios N. Dimitrakopoulos, P. Vlamos
 
 %load BCT package
-% addpath('C:\MATLAB\2019_03_03_BCT');
+% addpath('C:\MATLAB\2019_03_03_BCT'); %change to appropriate folder
 
-% the graph can be found at:
+% Using EEGLAB and SIFT plugin , the graph can be found at:
 % PDC = EEG.CAT.Conn.PDC;
 
-% freq = 10;
-% G = mean(PDC(:,:,freq,:), 4); %mean accross time
+%get graph at 7 Hz
+%freq = 7;
+%G = mean(PDC(:,:,freq,:), 4); %mean accross time (4th dimension)
+
 
 %load graph
 load('graph.mat');
 
 %set sparsity level
 sparsity = 0.2;
+%use BCT function to apply sparsity level
 GS = threshold_proportional(G, sparsity);
 
-%check if all nodes are connected in one component
+%check number of connected components
 c = graphconncomp(GS, 'weak',1); %if c==1, there is one connected component, else we need larger value of sparsity
 
-%make it symmetric
+
+%make graph symmetric
 A = (GS>0)|(GS'>0);
 GS(A) = G(A);
 
-%calculate Clustering Coefficient
-CC = mean(clustering_coef_bu(GS));
+%calculate clustering cofficient (CC)
+CC_all = clustering_coef_bu(GS); %CC per node
+CC = mean(CC); % average CC for graph
 
-%calculate Path Length
-Lengths = weight_conversion(GS,'lengths');
-% D: distances in 'steps'
-D = distance_bin(Lengths);
-%calculate characteristic path length, global efficiency 
-[L,Eff_glob] = charpath(D, 0, 0); 
+%get distancese between all nodes in "steps"
+Lengths = weight_conversion(GS, 'lengths');
+D = distance_bin(Lengths); 
+
+%calculate characteristic path length (L) and global efficiency
+[L, Eff_glob] = charpath(D, 0, 0); 
 
 %calculate local efficiency
-Eff_loc = mean(efficiency_bin(GS, 2));   
+Eff_loc_all = efficiency_bin(GS, 2); %Local efficiency for all nodes
+Eff_loc = mean(Eff_loc_all); %average local efficiency for graph
 
 %calculate betweenness centrality
 BC = mean(betweenness_bin(GS));
+
--- code-MATLAB/preprocessing.m
+++ code-MATLAB/preprocessing.m
@@ -1,11 +1,15 @@
 % Authors: Aristidis G. Vrahatis, Georgios N. Dimitrakopoulos, P. Vlamos
 
-eeglab; %start gui
-%load data and channel locations
+%start gui
+eeglab; 
+
+%load data
 EEG = pop_loadset('EEG_data_demo.set');
+
+%add chanlel location information from file
 EEG = pop_chanedit(EEG, 'load',{'chan_locs.ced', 'filetype', 'autodetect'});
 
-%set sampling rate
+%resample
 sampling_rate = 256;
 EEG = pop_resample(EEG,sampling_rate);
 
@@ -15,34 +19,36 @@ filter_high = 45;
 EEG = pop_eegfiltnew(EEG, filter_low, filter_high);
 
 %re-referencing
-ref= {'A1', 'A2'}; %re-rereference to mastoid channels
-%ref= []; %average reference
+ref= {'A1', 'A2'}; %using specific channels
+%ref= [];  %for average reference
 EEG = pop_reref(EEG, ref);
 
-%split to epochs based on events
+%dividing into epochs based on events (-1 sec to 3 sec after event '30')
 events = {'30'};
-timelimits = [-1, 3]; %-1 to 3 sec before/after the event
+timelimits = [-1, 3]; %time in s
 EEG = pop_epoch(EEG, events, timelimits);
 
-%remove baseline
-timerange = [-1000 0]; %in msec
+%remove baseline, -1000 ms to 0 ms relative to event
+timerange = [-1000 0]; %time in msec
 EEG = pop_rmbase( EEG, timerange);
 
-%select some trials
+%select data, here tials 1-10 
 EEG = pop_select(EEG, 'trial', 1:10);
 
-eeglab redraw; %refresh gui
+%refresh gui
+eeglab redraw; 
 
-%RUN Independent Component Aanlysis (ICA)
+%run ICA for artifact removal
 EEG = pop_runica(EEG, 'icatype', 'runica');
 
-%plot top 20 components
-pop_topoplot( EEG, 0, 1:20); %0 components, 1 erp
+%plot top 20 ICA components
+pop_topoplot(EEG, 0, 1:20);
 
-%inspect and select some for rejection.
-%subtract components
+%inspect previous figure and select some components for rejection (here 1-2):
 rej=1:2;
+
+%remove ICA components
 EEG = pop_subcomp(EEG, rej, 0);
 
-%save processed EEG data
+%save data
 pop_saveset( EEG, 'filename', 'clean_EEG.set');
